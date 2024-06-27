d508d8ddef1cca42d539b8e7f1ae312d1e6e65a0
raifa21 <helloiamkotaro@gmail.com>
2024-06-27 16:27:33 +0700

    Worked on aboutpage.

--- pages/aboutpage.tsx
+++ pages/aboutpage.tsx
@@ -2,11 +2,11 @@
 
 import { Indie_Flower } from "next/font/google";
 import PageHeader from "@/components/pageheader";
-import UseAnimations from "react-useanimations";
-import github from "react-useanimations/lib/github";
-import linkedin from "react-useanimations/lib/linkedin";
-import Link from "next/link";
+import Image from "next/image";
+import "./aboutpage.css";
 import "/styles/globals.css";
+
+import styles from "./IconGrid.module.css";
 
 const indieFlower = Indie_Flower({
   weight: "400",
@@ -18,30 +18,61 @@ export default function Home() {
     <>
       <PageHeader />
       <div className={`mainpage body ${indieFlower.className}`}>
-        <div className="snslinks">
-          <Link href="https://github.com/Raifa21" legacyBehavior passHref>
-            <a className="navlogos">
-              <UseAnimations
-                animation={github}
-                size={30}
-                strokeColor="black"
-                fillColor="blue"
-              />
-            </a>
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/kotaro-yoshizawa"
-            legacyBehavior
-            passHref
-          >
-            <a className="navlogos">
-              <UseAnimations
-                animation={linkedin}
-                size={30}
-                strokeColor="black"
-              />
-            </a>
-          </Link>
+        <div className="about-text">
+          <h1 className="title-text">About Me</h1>
+          <p className="content-text">
+            Hello! I am a Year 13 student with a deep passion for computer
+            science and engineering.
+            <br />
+            As a dedicated programmer, I excel in problem-solving and
+            continuously strive to enhance my skills. I am currently learning
+            web development, and I am excited to see where this journey takes
+            me.
+            <br /> With a strong foundation in programming and a keen interest
+            in innovation, I aim to create impactful solutions and provide
+            exceptional experiences.
+          </p>
+        </div>
+        <div className="skills-text">
+          <h1 className="title-text">Skills</h1>
+          <div className={styles.grid}>
+            <Image
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-plain.svg"
+              alt="TypeScript Icon"
+              width={64}
+              height={64}
+            />
+            <Image
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+              alt="React Icon"
+              width={64}
+              height={64}
+            />
+            <Image
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-plain.svg"
+              alt="Next.js Icon"
+              width={64}
+              height={64}
+            />
+            <Image
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-plain.svg"
+              alt="HTML5 Icon"
+              width={64}
+              height={64}
+            />
+            <Image
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-plain.svg"
+              alt="CSS3 Icon"
+              width={64}
+              height={64}
+            />
+            <Image
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-plain.svg"
+              alt="C++ Icon"
+              width={64}
+              height={64}
+            />
+          </div>
         </div>
         <div className="footer">
           <p>Design. Create. Repeat.</p>
